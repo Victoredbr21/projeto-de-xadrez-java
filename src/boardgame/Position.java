cfb11920a4d1fa4a85a6e7589f32881ff8838255
Victor Americo <victoroeste12@gmail.com>
2025-09-02 14:06:08 -0300

    primeira classe do sistema position criada

---
+++ src/boardgame/Position.java
@@ -0,0 +1,37 @@
+package boardgame;
+
+public class Position {
+    private int linha;
+    private int coluna;
+
+
+
+    public Position(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+       //getters
+        public int getLinha() {
+            return linha;
+        }
+
+        public int getColuna() {
+            return coluna;
+        }
+
+        //setters
+        public void setLinha(int linha) {
+            this.linha = linha;
+        }
+
+        public void setColuna(int coluna) {
+            this.coluna = coluna;
+        }
+
+@Override
+    public String toString() {
+        return  linha + "," + coluna;
+}
+
+}
